52316e4954477494a4d61ce9c8a8fcaeb020e35a
Justin Quinn <justin@willowmeadsoftwaresolutions.co.uk>
2022-05-18 13:17:30 +0100

    make static method

--- src/Program.cs
+++ src/Program.cs
@@ -3,7 +3,7 @@ using System.Diagnostics.CodeAnalysis;
 namespace weather
 {
     [ExcludeFromCodeCoverage]
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
